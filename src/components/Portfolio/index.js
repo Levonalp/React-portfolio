da938865f717f17b0ccc74972c02190d6ee32b31
levonal <lwshabee@gmail.com>
2023-02-17 13:11:07 -0600

    added target = blank

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -40,10 +40,18 @@ const Portfolio = () => {
           <div className="img-container">
             <img src={bookStore} alt="bookStore" />
             <div className="btns">
-              <a href="https://github.com/Levonalp/bookStore" target="_blank">
+              <a
+                href="https://github.com/Levonalp/bookStore"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <button className="btn">GitHub</button>
               </a>
-              <a href="https://react-bookstore.herokuapp.com/" target="_blank">
+              <a
+                href="https://react-bookstore.herokuapp.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <button className="btn">Demo</button>
               </a>
             </div>
@@ -54,12 +62,14 @@ const Portfolio = () => {
               <a
                 href="https://github.com/Levonalp/Travel-Log-React"
                 target="_blank"
+                rel="noopener noreferrer"
               >
                 <button className="btn">GitHub</button>
               </a>
               <a
                 href="https://www.loom.com/share/2b7a8c8eaf884a08bcd225c79e78ddde"
                 target="_blank"
+                rel="noopener noreferrer"
               >
                 <button className="btn">Demo</button>
               </a>
@@ -69,12 +79,17 @@ const Portfolio = () => {
           <div className="img-container">
             <img src={notesApp} alt="bookStore" />
             <div className="btns">
-              <a href="https://github.com/Levonalp/Notepad_api" target="_blank">
+              <a
+                href="https://github.com/Levonalp/Notepad_api"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <button className="btn">GitHub</button>
               </a>
               <a
                 href="https://blooming-oasis-43438.herokuapp.com/"
                 target="_blank"
+                rel="noopener noreferrer"
               >
                 <button className="btn">Demo</button>
               </a>
@@ -86,10 +101,15 @@ const Portfolio = () => {
               <a
                 href="https://github.com/Levonalp/dog_project4"
                 target="_blank"
+                rel="noopener noreferrer"
               >
                 <button className="btn">GitHub</button>
               </a>
-              <a href="https://urldogshowback.herokuapp.com/" target="_blank">
+              <a
+                href="https://urldogshowback.herokuapp.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <button className="btn">Demo</button>
               </a>
             </div>
@@ -97,12 +117,17 @@ const Portfolio = () => {
           <div className="img-container">
             <img src={port} alt="bookStore" />
             <div className="btns">
-              <a href="https://github.com/Levonalp/Portfolio" target="_blank">
+              <a
+                href="https://github.com/Levonalp/Portfolio"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <button className="btn">GitHub</button>
               </a>
               <a
                 href="https://6369af711cf693453eb2e670--transcendent-fenglisu-a3a9fb.netlify.app/"
                 target="_blank"
+                rel="noopener noreferrer"
               >
                 <button className="btn">Demo</button>
               </a>
@@ -114,12 +139,14 @@ const Portfolio = () => {
               <a
                 href="https://github.com/Levonalp/project-3-backend"
                 target="_blank"
+                rel="noopener noreferrer"
               >
                 <button className="btn">GitHub</button>
               </a>
               <a
                 href="https://twitter-clone-frontend123.herokuapp.com/#"
                 target="_blank"
+                rel="noopener noreferrer"
               >
                 <button className="btn">Demo</button>
               </a>
@@ -131,12 +158,14 @@ const Portfolio = () => {
               <a
                 href="https://github.com/Levonalp/Full-crud-app"
                 target="_blank"
+                rel="noopener noreferrer"
               >
                 <button className="btn">GitHub</button>
               </a>
               <a
                 href="https://www.loom.com/share/67bac857a0d64787ac48e5788f9e3264"
                 target="_blank"
+                rel="noopener noreferrer"
               >
                 <button className="btn">Demo</button>
               </a>
@@ -148,10 +177,15 @@ const Portfolio = () => {
               <a
                 href="https://github.com/Levonalp/React-portfolio"
                 target="_blank"
+                rel="noopener noreferrer"
               >
                 <button className="btn">GitHub</button>
               </a>
-              <a href="https://levonalshabi.netlify.app/" target="_blank">
+              <a
+                href="https://levonalshabi.netlify.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <button className="btn">Demo</button>
               </a>
             </div>
