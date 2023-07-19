825c39f1257ce7b90575ef851fe70c8c9c135f42
levonal <lwshabee@gmail.com>
2023-07-19 16:13:31 -0500

    fix page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -26,37 +26,41 @@ const About = () => {
   return (
     <>
       <div className="container about-page">
-        <div className="text-zone">
-          <h1>
-            <AnimatedLetters
-              letterClass={letterClass}
-              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
-              idx={15}
-            />
-          </h1>
-          <p className="abt">
-            As a full-stack software developer, I am fueled by my passion for
-            solving complex technical problems and finding the most efficient
-            solutions.
-          </p>
-          <p className="abt">
-            My skills in the MERN and PERN stacks are constantly evolving as I
-            strive to implement cutting-edge features. My background in sales
-            and real estate appraising has honed my ability to thrive in
-            fast-paced environments and effectively utilize my skills.
-          </p>
-          <p className="abt">
-            I am on a mission to find roles where I can collaborate with others
-            to research and develop front-end and back-end products that truly
-            exceed customer expectations. My ultimate goal is to make a
-            meaningful impact through our collective efforts and not just to
-            complete tasks.
-          </p>
-          <br />
+        <div className="half-width">
+          <div className="text-zone">
+            <h1>
+              <AnimatedLetters
+                letterClass={letterClass}
+                strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+                idx={15}
+              />
+            </h1>
+            <p className="abt">
+              As a full-stack software developer, I am fueled by my passion for
+              solving complex technical problems and finding the most efficient
+              solutions.
+            </p>
+            <p className="abt">
+              My skills in the MERN and PERN stacks are constantly evolving as I
+              strive to implement cutting-edge features. My background in sales
+              and real estate appraising has honed my ability to thrive in
+              fast-paced environments and effectively utilize my skills.
+            </p>
+            <p className="abt">
+              I am on a mission to find roles where I can collaborate with
+              others to research and develop front-end and back-end products
+              that truly exceed customer expectations. My ultimate goal is to
+              make a meaningful impact through our collective efforts and not
+              just to complete tasks.
+            </p>
+            <br />
+          </div>
         </div>
 
         <div>
-          <img className="proimg" src={proimg}></img>
+          <div className="half-width image-zone">
+            <img className="proimg" src={proimg}></img>
+          </div>
         </div>
       </div>
       <Loader type="pacman" />
